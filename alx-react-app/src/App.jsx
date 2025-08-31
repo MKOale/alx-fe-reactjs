075a445ffdd441aaa4fd8275eadb291a4ef6ccbb
Kolajo Ale <mobolajikolajoolamide@gmail.com>
2025-08-31 21:36:19 +0100

    Added WelcomeMessage component

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,6 +3,9 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+// import your new component
+import WelcomeMessage from './components/WelcomeMessage'
+
 function App() {
   const [count, setCount] = useState(0)
 
@@ -28,6 +31,9 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+
+      {/* Your new component will render here */}
+      <WelcomeMessage />
     </>
   )
 }
---
+++ alx-react-app/src/components/WelcomeMessage.jsx
@@ -0,0 +1,5 @@
+function WelcomeMessage() {
+  return <h1>Welcome to my first React App!</h1>;
+}
+
+export default WelcomeMessage;
