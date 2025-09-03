981f1ef2d3147761a432215624c9624cb99981f3
Kolajo Ale <mobolajikolajoolamide@gmail.com>
2025-09-03 16:47:41 +0100

    Implement context with UserContext, UserProfile, and App update

--- alx-react-app-props/src/UserContext.js
+++ alx-react-app-props/src/UserContext.js
@@ -1,5 +1,8 @@
+// src/UserContext.js
 import { createContext } from "react";
 
+// Create the context
 const UserContext = createContext();
 
+// Export it as default
 export default UserContext;
--- alx-react-app-props/src/UserInfo.jsx
+++ alx-react-app-props/src/UserInfo.jsx
@@ -1,5 +1,5 @@
 // src/UserInfo.jsx
-import UserDetails from './UserDetails';
+import UserDetails from './UserProfile';
 
 function UserInfo() {
   return <UserDetails />;
