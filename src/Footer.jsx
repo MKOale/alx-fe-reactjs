82212f5f73b3e8349eb8f23d795390791adda6b2
Kolajo Ale <mobolajikolajoolamide@gmail.com>
2025-09-03 18:26:06 +0100

    Task 3: Added React Router setup with Navbar, pages, and footer

---
+++ src/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer style={{ marginTop: "20px", padding: "10px", background: "#eee" }}>
+      <p>© 2025 My Company. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
