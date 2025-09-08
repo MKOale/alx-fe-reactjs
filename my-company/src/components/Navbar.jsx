e271795eee70e2ff02e8e360c9a7e44233a515eb
Kolajo Ale <mobolajikolajoolamide@gmail.com>
2025-09-08 20:17:26 +0100

    Add inline styling with backgroundColor to Navbar

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -4,17 +4,24 @@ function Navbar() {
   return (
     <nav
       style={{
-        background: "#333",
+        backgroundColor: "#333",
         padding: "10px",
         display: "flex",
-        justifyContent: "center",
-        gap: "20px"
+        gap: "15px",
       }}
     >
-      <Link to="/" style={{ color: "#fff", textDecoration: "none" }}>Home</Link>
-      <Link to="/about" style={{ color: "#fff", textDecoration: "none" }}>About</Link>
-      <Link to="/services" style={{ color: "#fff", textDecoration: "none" }}>Services</Link>
-      <Link to="/contact" style={{ color: "#fff", textDecoration: "none" }}>Contact</Link>
+      <Link to="/" style={{ color: "white", textDecoration: "none" }}>
+        Home
+      </Link>
+      <Link to="/about" style={{ color: "white", textDecoration: "none" }}>
+        About
+      </Link>
+      <Link to="/services" style={{ color: "white", textDecoration: "none" }}>
+        Services
+      </Link>
+      <Link to="/contact" style={{ color: "white", textDecoration: "none" }}>
+        Contact
+      </Link>
     </nav>
   );
 }
