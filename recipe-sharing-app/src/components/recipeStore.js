6030a8b3a79cf78fa2093e975578b0642819b3f8
Kolajo Ale <mobolajikolajoolamide@gmail.com>
2025-09-15 11:47:32 +0100

    Feat: add updateRecipe and deleteRecipe to recipeStore (T1, T2)

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -3,10 +3,28 @@ import { create } from "zustand";
 const useRecipeStore = create((set) => ({
   recipes: [],
   filteredRecipes: [],
-  setRecipes: (recipes) => set({ recipes }),
+
+  // Add a new recipe
   addRecipe: (recipe) =>
     set((state) => ({
       recipes: [...state.recipes, recipe],
+    })),
+
+  // Set/replace recipes
+  setRecipes: (recipes) => set({ recipes }),
+
+  // Update a recipe by id
+  updateRecipe: (updatedRecipe) =>
+    set((state) => ({
+      recipes: state.recipes.map((recipe) =>
+        recipe.id === updatedRecipe.id ? updatedRecipe : recipe
+      ),
+    })),
+
+  // Delete a recipe by id
+  deleteRecipe: (id) =>
+    set((state) => ({
+      recipes: state.recipes.filter((recipe) => recipe.id !== id),
     })),
 }));
 
