6e4b7f8d270cc2da05341de1e60613d1949bf09e
Kolajo Ale <mobolajikolajoolamide@gmail.com>
2025-09-15 11:15:37 +0100

    Fix: addRecipe function added to recipeStore for T0

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,26 +1,13 @@
-import create from 'zustand';
+import { create } from "zustand";
 
-export const useRecipeStore = create((set, get) => ({
-  recipes: [], 
-  favorites: [], 
-
-  setRecipes: (recipes) => set({ recipes }), // 👈 new line
-
-  addFavorite: (recipeId) => set(state => ({
-    favorites: [...new Set([...state.favorites, recipeId])]
-  })),
-
-  removeFavorite: (recipeId) => set(state => ({
-    favorites: state.favorites.filter(id => id !== recipeId)
-  })),
-
-  recommendations: [],
-
-  generateRecommendations: () => {
-    const { recipes, favorites } = get();
-    const recommended = recipes.filter(recipe =>
-      !favorites.includes(recipe.id) && Math.random() > 0.5
-    );
-    set({ recommendations: recommended });
-  },
+const useRecipeStore = create((set) => ({
+  recipes: [],
+  filteredRecipes: [],
+  setRecipes: (recipes) => set({ recipes }),
+  addRecipe: (recipe) =>
+    set((state) => ({
+      recipes: [...state.recipes, recipe],
+    })),
 }));
+
+export default useRecipeStore;
